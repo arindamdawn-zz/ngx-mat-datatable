2bf9299d2f965c596dd85ff93f674bbcc8e3092b
arindamdawn <arindamdawn3@gmail.com>
2020-04-06 13:27:38 +0530

    Added sample app

--- projects/shimmer-loader/README.md
+++
@@ -1,3 +0,0 @@
-# ShimmerLoader
-
-Configurable shimmer loader which can be used to mimic the structure of lists, tables etc while the data is loading
--- projects/shimmer-loader/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of shimmer-loader
- */
-
-export * from "./lib/shimmer-loader.service";
-export * from "./lib/shimmer-loader.component";
-export * from "./lib/shimmer-loader.module";
